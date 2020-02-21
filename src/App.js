5e6d472070b4270e8d4e70fcb32f68b34d9bc6fa
milochen0418 <milo_chen@mail2000.com.tw>
2020-02-21 11:37:20 +0800

    Use CounterContainer in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import CounterContainer from "./CounterContainer";
 import { render } from "react-dom"; //Only just import react-dom.render() function
 
 const App = () => {
   return (
     <div>
+      <CounterContainer />
       <h1 id="something-important">Adopt Me!</h1>
     </div>
   );
